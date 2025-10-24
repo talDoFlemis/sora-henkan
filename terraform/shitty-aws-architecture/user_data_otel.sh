7ea74b91f002588780f23d446dea021e71d7b5a1
Said Rodrigues <coderflemis@gmail.com>
2025-10-24 13:03:42 -0300

    feat: add cloudwatch-agent to ec2 instances

--- terraform/shitty-aws-architecture/user_data_otel.sh
+++ terraform/shitty-aws-architecture/user_data_otel.sh
@@ -9,6 +9,9 @@ yum install -y docker
 systemctl start docker
 systemctl enable docker
 
+# Install cloudwatch agent
+yum install amazon-cloudwatch-agent
+
 # Install Docker Compose
 curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
