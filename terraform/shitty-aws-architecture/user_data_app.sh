80afaed0169b615bcc7c7b2c4493fdf298e75cb3
Said Rodrigues <coderflemis@gmail.com>
2025-10-24 13:42:13 -0300

    fix(user_data): missing -y to cloudwatch agent

--- terraform/shitty-aws-architecture/user_data_app.sh
+++ terraform/shitty-aws-architecture/user_data_app.sh
@@ -10,7 +10,7 @@ systemctl start docker
 systemctl enable docker
 
 # Install cloudwatch agent
-yum install amazon-cloudwatch-agent
+yum install -y amazon-cloudwatch-agent
 
 # Install Docker Compose
 curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
