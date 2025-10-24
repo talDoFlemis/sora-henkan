79bd830896e9347d60ea2a9de746f20158e4219a
Said Rodrigues <coderflemis@gmail.com>
2025-10-24 06:44:29 -0300

    feat(s3): make bucket public

--- terraform/shitty-aws-architecture/06-s3.tf
+++ terraform/shitty-aws-architecture/06-s3.tf
@@ -34,10 +34,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "images" {
 resource "aws_s3_bucket_public_access_block" "images" {
   bucket = aws_s3_bucket.images.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 # S3 Bucket Lifecycle Configuration
